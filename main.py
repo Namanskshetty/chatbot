f6c907c3c0307bc1fb46bd3404ddd13b451fa7bb
Namanskshetty <64603506+Namanskshetty@users.noreply.github.com>
2021-05-29 14:03:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request  # Import Flask Class, and render_template
+from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from pytz import timezone
@@ -7,15 +7,17 @@ import datetime
 import wikipedia
 import pyjokes
 import emoji
-import random
 import os
+import random
 import requests
 import json
 
+#  pip install chatterbot==1.0.2
 app = Flask(__name__)  # Create an Instance
-english_bot = ChatBot("ChatterBot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
-                      
+english_bot = ChatBot("ChatterBot",
+                      storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
+#trainig the data
 trainer = ChatterBotCorpusTrainer(english_bot)
 trainer.train("chatterbot.corpus.english")
 trainer.train("data/data.yml")
@@ -32,45 +34,79 @@ def get_bot_response():
     print(str(userText))
     small = userText.lower()
     if "time" in small:
-        yet = str(datetime.datetime.now(pytz.timezone('Asia/Kolkata'))) #for timezone refer(https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
+        yet = str(datetime.datetime.now(pytz.timezone('Asia/Kolkata')))
         return "Time is " + yet
     elif "joke" in small:
-      return str(pyjokes.get_joke())
+        return str(pyjokes.get_joke())
+    elif "help" in small:
+      naman=str(random.choice(["hello","nice to meet you","HI","oh!! help is right here","Let's see","hey"]))
+      new=str(" User\n\n you can use chat bot to have normal conversations like, aking time, jokes,quotes and much more\n to know more about the chatbot check for code by asking for he bot\n\n NOTE: WE ARE WOKING ON THE DOCUMENTATION PAGE")
+      hi=naman+new
+      return hi
+    elif small in ["hi","hello"]:
+      aman=str(random.choice(["!!!","nice to meet you","how may I help you","User"]))
+      yet = str(datetime.datetime.now(pytz.timezone('Asia/Kolkata')))
+      seo=int(yet[11:13])
+      print(seo)
+      if seo>=0 and seo<12:
+        new=str("Good morning ")
+        nama=str(aman)
+        kai=new+nama
+        return kai
+      elif seo>=12 and seo<16:
+        new=str("Good afternoon ")
+        nama=str(aman)
+        kai=new+nama
+        return kai
+      elif seo>=16 and seo<19:
+        new=str("Good evening ")
+        nama=str(aman)
+        kai=new+nama
+        return kai 
+      elif seo>=19 and seo<22:
+        new=str("Good night ")
+        nama=str(aman)
+        kai=new+nama
+        return kai
+      else: 
+        return "Good night have a nice sleep"
     elif "random" in small:
-      try:
-          num=[int(word) for word in small.split() if word.isdigit()]
-          fi=num[0]
-          se=num[1]
-          return str(random.randrange(fi,se))
-      except:
-          return str("random number>",random.randrange(0,1000),"<(Predefined range/Default Range)")
-
-    elif "who is" in small:
-      person = small.replace('who is', '')
-      return str(wikipedia.summary(person, 1))
-    elif "created you" in small:
-      return emoji.emojize("Naman :red_heart:")
-    elif "wikki" in small:
-      word= small.replace("wikki","")
-      if "search" in word:
-        sear = word.replace('search', '')
-        return str(wikipedia.search(sear))
-      elif "page" in word:
         try:
-          seaar = word.replace('page', '')
-          ny = wikipedia.summary(seaar)
-          return str(ny)
+            num = [int(word) for word in small.split() if word.isdigit()]
+            fi = num[0]
+            se = num[1]
+            return str(random.randrange(fi, se))
         except:
-          return "Result not found!!(try typing wikki search ____)"
-      else:
-        return emoji.emojize("sorry :face_with_monocle: use page or search with this")
+            return str("random number>", random.randrange(0, 1000),
+                       "<(Predefined range/Default Range)")
+    elif "who is" in small:
+        person = small.replace('who is', '')
+        return str(wikipedia.summary(person, 1))
+    elif "created you" in small:
+        return emoji.emojize("Naman :red_heart:")
+    elif "wikki" in small:
+        word = small.replace("wikki", "")
+        if "search" in word:
+            sear = word.replace('search', '')
+            return str(wikipedia.search(sear))
+        elif "page" in word:
+            try:
+                seaar = word.replace('page', '')
+                ny = wikipedia.summary(seaar)
+                return str(ny)
+            except:
+                return "Result not found!!(try typing wikki search ____)"
+        else:
+            return emoji.emojize(
+                "sorry :face_with_monocle: use page or search with this")
     elif "quote" in small:
-      response = requests.get("https://zenquotes.io/api/random")
-      json_data = json.loads(response.text)
-      quote = json_data[0]['q'] + " -" + json_data[0]['a']
-      return str(quote)
+        response = requests.get("https://zenquotes.io/api/random")
+        json_data = json.loads(response.text)
+        quote = json_data[0]['q'] + " -" + json_data[0]['a']
+        return str(quote)
     else:
-      return str(english_bot.get_response(userText))
+        return str(english_bot.get_response(userText))
 
 
+#hosts at the ip
 app.run(host='0.0.0.0', port=5000, debug=True)
